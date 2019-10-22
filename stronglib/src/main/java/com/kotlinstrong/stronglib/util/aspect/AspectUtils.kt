ad114ec6336e077c1b01a0613440d453b0fb93d6
LiuZhen <1024477951@qq.com>
2019-10-22 18:32:01 +0800

    AspectUtils

--- stronglib/src/main/java/com/kotlinstrong/stronglib/util/aspect/AspectUtils.kt
+++ stronglib/src/main/java/com/kotlinstrong/stronglib/util/aspect/AspectUtils.kt
@@ -5,6 +5,7 @@ import org.aspectj.lang.annotation.Aspect
 import org.aspectj.lang.JoinPoint
 import org.aspectj.lang.annotation.AfterReturning
 import org.aspectj.lang.annotation.Before
+import org.aspectj.lang.annotation.Pointcut
 
 
 @Aspect
@@ -18,6 +19,14 @@ open class AspectUtils {
      *  Around:在方法执行前后和抛出异常时执行，前面的综合
      * */
 
+    /** execution test方法里切入前后，call test方法外切入前后。
+     * 匹配所有public的方法：execution(public * *(..))
+     * 匹配包下所有类的方法：execution(* com.kotlin.*(..)) （一个点表示不包含子包）
+     * execution(* com.kotlin.. *(..)) （两个点表示包含子包）
+     * 匹配实现特定接口的所有类的方法：execution(* com.kotlin.xxinterface+.*(..))
+     * 匹配所有test开头的方法：execution(* test*(..))
+     * 匹配所有方法：execution(* *.*(..))
+     * */
     @Before("execution(* com.kotlinstrong.main.MainActivity.test*(..))")
     fun testAspectBefore(point: JoinPoint) {
         LogUtils.e("AspectUtils $point.signature.name -Before")
@@ -27,4 +36,22 @@ open class AspectUtils {
     fun testAspectAfterReturning(point: JoinPoint,id:Int) {
         LogUtils.e("AspectUtils $point.signature.name -AfterReturning $id")
     }
+
+    // 同时调用两个test方法才会触发
+//    @Pointcut("testAspect() && testAfterReturning()")
+//    fun doubleMethod(point: JoinPoint) {
+//        LogUtils.e("AspectUtils $point.signature.name -doubleMethod")
+//    }
+
+//    @Before("doubleMethod()")
+//    fun beforeDoubleMethod(point: JoinPoint) {
+//        val key = point.signature.toString()
+//        LogUtils.e("AspectUtils $key -beforeDoubleMethod")
+//    }
+
+//    @MyAspectAnnotation
+//    fun cusTomMethod(point: JoinPoint) {
+//        val key = point.signature.toString()
+//        LogUtils.e("AspectUtils $key -beforeDoubleMethod")
+//    }
 }
